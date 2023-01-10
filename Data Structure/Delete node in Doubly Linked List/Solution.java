769ea9efa964b4878bf36bc69a667f812ca6faaa
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-10 21:08:41 +0530

    Create Solution.java

---
+++ Data Structure/Delete node in Doubly Linked List/Solution.java
@@ -0,0 +1,49 @@
+// Delete node in Doubly Linked List
+// Given a doubly linked list and a position. The task is to delete a node from given position in a doubly linked list.
+
+// Example 1:
+
+// Input:
+// LinkedList = 1 <--> 3 <--> 4 
+// x = 3
+// Output: 1 3  
+// Explanation: After deleting the node at
+// position 3 (position starts from 1),
+// the linked list will be now as 1->3.
+// Example 2:
+
+// Input:
+// LinkedList = 1 <--> 5 <--> 2 <--> 9  
+// x = 1
+// Output: 5 2 9
+// Your Task:
+// The task is to complete the function deleteNode() which should delete the node at given position and return the head of the linkedlist.
+
+// Expected Time Complexity : O(N)
+// Expected Auxilliary Space : O(1)
+
+// Constraints:
+// 2 <= size of the linked list <= 1000
+// 1 <= x <= N
+
+ 
+class Solution
+{
+    // function returns the head of the linkedlist
+    Node deleteNode(Node head,int x)
+    {
+        if(x ==1) {
+            head = head.next;
+            return head;
+        }
+	// Your code here	
+	x--;
+	Node temp = head;
+	while(x != 1) {
+	    x--;
+	    temp = temp.next;
+	}
+	temp.next = temp.next.next;
+	return head;
+    }
+}
