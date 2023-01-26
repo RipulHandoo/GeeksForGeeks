c17d882dc1f75c707dc43c9e5b31060d65f8e784
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-26 22:19:58 +0530

    Update Solution.java

--- Data Structure/Level order traversal/Solution.java
+++ Data Structure/Level order traversal/Solution.java
@@ -1,1 +1,43 @@
 
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to return the level order traversal of a tree.
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+        // Your code here
+        if(node== null) {
+            return new ArrayList<Integer>();
+        }
+        
+        Queue<Node> q = new ArrayDeque<>();
+        
+        q.add(node);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        while(!q.isEmpty()){
+            Node temp = q.remove();
+            list.add(temp.data);
+            if(temp.left != null) {
+                q.add(temp.left);
+            }
+            if(temp.right != null) {
+                q.add(temp.right);
+            }
+        }
+        return list;
+    }
+}
