cc22ea591cf14d1ff88484339bc3bffebba00654
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-27 23:42:35 +0530

    Update Solution.java

--- Data Structure/Height of Binary Tree/Solution.java
+++ Data Structure/Height of Binary Tree/Solution.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        // code here 
+        if(node == null) {
+            return 0;
+        }
+        int left = height(node.left);
+        int right = height(node.right);
+        return  max(left,right) + 1;
+    }
+    
+    int max(int left, int right) {
+        if(left > right) {
+            return left;
+        }
+        else {
+            return right;
+        }
+    }
+}
