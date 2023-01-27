9ad86826069c8dadade8fc97eaa602fb93380b4c
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-27 23:01:56 +0530

    Update Solution.java

--- Data Structure/Majority Element/Solution.java
+++ Data Structure/Majority Element/Solution.java
@@ -1,1 +1,26 @@
 
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i = 0;i<size;i++) {
+            if(map.containsKey(a[i])) {
+                map.put(a[i],map.get(a[i]) + 1);
+            }
+            if(!map.containsKey(a[i])) {
+                map.put(a[i],1);
+            }
+        }
+        
+//       this loop is used to traverse the HashMap
+        for(Map.Entry<Integer,Integer> e : map.entrySet()) {
+            if(e.getValue() > (a.length/2)) {
+                return e.getKey();
+            }
+        }
+        return -1;
+    }
+}
