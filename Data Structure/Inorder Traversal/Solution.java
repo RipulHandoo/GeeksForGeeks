345a7261141be151a331a151dac594c5b0907aa0
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-28 22:59:02 +0530

    Update Solution.java

--- Data Structure/Inorder Traversal/Solution.java
+++ Data Structure/Inorder Traversal/Solution.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> inOrder(Node root) {
+        // Code
+        if(root == null) {
+            return list;
+        }
+        inOrder(root.left);
+        list.add(root.data);
+        inOrder(root.right);
+        return list;
+    }
+}
