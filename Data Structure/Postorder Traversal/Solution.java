3ebaca6a49f849f8c8b68abc88a04829e67f09c6
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-29 22:43:15 +0530

    Update Solution.java

--- Data Structure/Postorder Traversal/Solution.java
+++ Data Structure/Postorder Traversal/Solution.java
@@ -1,1 +1,26 @@
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
 
+class Tree
+{
+    //Function to return a list containing the postorder traversal of the tree.
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    ArrayList<Integer> postOrder(Node root)
+    {
+       // Your code goes here
+       if(root == null) {
+           return list;
+       }
+       postOrder(root.left);
+       postOrder(root.right);
+       list.add(root.data);
+       
+       return list;
+    }
+}
