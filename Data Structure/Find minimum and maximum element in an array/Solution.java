5f4cba77732618d76e7ea3a17866711eedb04e61
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-01-29 22:17:17 +0530

    Update Solution.java

--- Data Structure/Find minimum and maximum element in an array/Solution.java
+++ Data Structure/Find minimum and maximum element in an array/Solution.java
@@ -1,1 +1,32 @@
+ class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+        long min = a[0];
+        long max = a[0];
+        
+        for(int i = 0;i<n;i++) {
+            if(a[i] > max) {
+                max = a[i];
+            }
+            if(a[i] < min) {
+                min = a[i];
+            }
+        }
+        
+        pair z = new pair(min, max);
+        return z;
+    }
+}
 
