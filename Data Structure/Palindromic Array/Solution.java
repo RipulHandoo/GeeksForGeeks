ad9932f858062a1fb44a9fea8f8db3a6479ff7bc
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-06 20:25:01 +0530

    Update Solution.java

--- Data Structure/Palindromic Array/Solution.java
+++ Data Structure/Palindromic Array/Solution.java
@@ -1,1 +1,23 @@
-
+class GfG
+{
+	public static int palinArray(int[] a, int n)
+   {
+          //add code here.
+      int flag = 1;
+      for(int i = 0;i<n;i++) {
+          int num_copy = a[i];
+          int temp = 0;
+          int reverse = 0;
+          while(num_copy > 0) {
+              temp = num_copy%10;
+              reverse = reverse*10 + temp;
+              num_copy = num_copy/10;
+          }
+          if(reverse != a[i]) {
+              flag = 0;
+              break;
+          }
+      }
+      return flag;
+   }
+}
