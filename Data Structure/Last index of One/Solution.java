c1095b462a39306b673c0f98dbcad448d3ea8f14
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-07 21:56:56 +0530

    Update Solution.java

--- Data Structure/Last index of One/Solution.java
+++ Data Structure/Last index of One/Solution.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int lastIndex( String s) {
+        
+        int last = -1;
+        for(int i = 0;i<s.length();i++) {
+            if(s.charAt(i) == '1') {
+                last = i;
+            }
+        }
+        return last;
+    }
+}
