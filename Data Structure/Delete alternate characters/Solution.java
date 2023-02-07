95a2fd7e869512d2b7541a42a7b3c7834e8f630d
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-07 22:16:34 +0530

    Update Solution.java

--- Data Structure/Delete alternate characters/Solution.java
+++ Data Structure/Delete alternate characters/Solution.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    static String delAlternate(String S) {
+        // code here
+        String str = new String();
+        for(int i =0;i<S.length();i++) {
+           str = str + S.charAt(i);
+            i++;
+        }
+        return str;
+    }
+}
