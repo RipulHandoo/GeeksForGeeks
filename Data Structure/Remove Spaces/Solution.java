282a117d7a3031aa60723fe14c94c9aa930d3232
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-08 23:08:48 +0530

    Update Solution.java

--- Data Structure/Remove Spaces/Solution.java
+++ Data Structure/Remove Spaces/Solution.java
@@ -1,1 +1,15 @@
-
+class Solution
+{
+   
+    String modify(String S)
+    {
+        // your code here
+        String str = new String();
+        for(int i = 0;i<S.length();i++) {
+            if(S.charAt(i) != ' ') {
+                str += S.charAt(i);
+            }
+        }
+        return str;
+    }
+}
