6cceae6248654298cdc989b408d3e806b6a196c5
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-09 21:59:13 +0530

    Update Solution.java

--- Data Structure/Check String/Solution.java
+++ Data Structure/Check String/Solution.java
@@ -1,1 +1,16 @@
-
+class Sol
+{
+    Boolean check (String s)
+    {
+        // your code here      
+        boolean flag = true;
+        // check if the first character of the string is eqaul to all the other character 
+        // if not than return true else return false
+        for(int j = 1;j<s.length();j++){
+            if(s.charAt(0) != s.charAt(j)) {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+}
