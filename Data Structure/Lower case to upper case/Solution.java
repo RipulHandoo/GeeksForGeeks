be121560ee890767832da688cc0b12e93d0fd215
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-09 22:16:06 +0530

    Update Solution.java

--- Data Structure/Lower case to upper case/Solution.java
+++ Data Structure/Lower case to upper case/Solution.java
@@ -1,1 +1,12 @@
-
+class Solution 
+{ 
+    String to_upper(String str) 
+    { 
+        // code here
+        
+        //simply use the in built method
+        // toUpperCase to convert the lower case into upper case
+        String newStr = str.toUpperCase();
+        return newStr;
+    }
+} 
