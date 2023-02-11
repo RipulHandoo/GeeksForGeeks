7a4f8c358d62ba2f7d0f27cb2a9159913092b971
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-11 22:15:04 +0530

    Update Solution.java

--- Data Structure/Left View of Binary Tree/Solution.java
+++ Data Structure/Left View of Binary Tree/Solution.java
@@ -1,1 +1,26 @@
-
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    int max_level = 0;
+    ArrayList<Integer> leftView(Node root)
+    {
+      // Your code here
+      ArrayList<Integer> result = new ArrayList<Integer>();
+      view(root,result,1);
+      return result;
+    }
+    
+    void view(Node root, ArrayList<Integer> result, int level) {
+        if(root == null) {
+            return;
+        }
+        
+        if(level > max_level) {
+            result.add(root.data);
+            max_level = level;
+        }
+        
+        view(root.left,result,level+1);
+        view(root.right,result,level+1);
+    }
+}
