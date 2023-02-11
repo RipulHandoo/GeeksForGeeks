3f7d7165c4c4e4734c3a02e4754e2823c9f0aaee
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-11 22:22:15 +0530

    Update Solution.java

--- Data Structure/Check for BST/Solution.java
+++ Data Structure/Check for BST/Solution.java
@@ -1,1 +1,21 @@
-
+public class Solution
+{
+    //Function to check whether a Binary Tree is BST or not.
+    boolean isBST(Node root)
+    {
+        // code here.
+        return checkBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+    boolean checkBST(Node root, int min, int max) {
+        if(root == null) {
+            return true;
+        }
+        
+        if(root.data < min || root.data > max) {
+            return false;
+        }
+        
+        return(checkBST(root.left,min,root.data-1)&&
+        checkBST(root.right,root.data+1,max));
+    }
+}
