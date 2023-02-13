94c0d8ef09593049856a1cc5be467b4e3231b524
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-13 22:54:59 +0530

    Second approach
    What we can do in second approach is that,
    we'll create a stack and put all the elements of array in stack and 
    use a loop to pop the stack elements and compare them with array elements 
    if all elements matches that return true, else returns false

--- Data Structure/Perfect Arrays/Solution.java
+++ Data Structure/Perfect Arrays/Solution.java
@@ -1,1 +1,18 @@
-
+class Solution{
+    
+    public boolean IsPerfect(int a[], int n)
+    {
+        // Complete the function
+        
+        // for doing it in O(1) space complexity, we can reverse the array and than compare each element
+        boolean flag = true;
+        for(int i = 0;i<=n/2;i++) {
+            if(a[i] != a[n-i-1]) {
+                flag = false;
+                return flag;
+            }
+        }
+        return flag;
+    }
+    
+}
