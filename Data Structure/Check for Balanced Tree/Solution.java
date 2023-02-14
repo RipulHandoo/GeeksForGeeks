9eec524b5e909f0b0e98eb0221d306dccca13c53
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-14 22:39:51 +0530

    Update Solution.java

--- Data Structure/Check for Balanced Tree/Solution.java
+++ Data Structure/Check for Balanced Tree/Solution.java
@@ -1,1 +1,28 @@
+class Tree
+{
+    
+    boolean isBalanced(Node root) {
+        if (root == null)
+            return true;
 
+        /* Get the height of left and right sub-trees */
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(root.left) && isBalanced(root.right))
+            return true;
+
+        return false;
+    }
+     int height(Node node) {
+        if (node == null)
+            return 0;
+
+        /* Recursively calculate the height of left and right sub-trees */
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        /* Return the maximum of left and right sub-tree heights plus 1 */
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
