c6629b992123518c99b71b366aefb9863611b48d
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-16 21:07:23 +0530

    Update Solution.java

--- Data Structure/Insert a node in a BST/Solution.java
+++ Data Structure/Insert a node in a BST/Solution.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    // Function to insert a node in a BST.
+    Node insert(Node root, int Key) {
+        // your code here
+        if(root == null) {
+            root = new Node(Key);
+            return root;
+        }
+        if(root.data > Key) {
+            root.left = insert(root.left,Key);
+        }
+        else if(root.data < Key){
+            root.right = insert(root.right,Key);
+        }
+        return root;
+    }
+}
