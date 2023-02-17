8d7564e9a49d07bb9497674f9ac28e8dd2349b3e
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-17 23:21:54 +0530

    Update Solution.java

--- Data Structure/Reverse Level Order Traversal/Solution.java
+++ Data Structure/Reverse Level Order Traversal/Solution.java
@@ -1,1 +1,20 @@
-
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()) {
+            Node temp = q.poll();
+            list.add(0, temp.data);
+            if(temp.right != null) {
+                q.add(temp.right);
+            }
+            if(temp.left != null) {
+                q.add(temp.left);
+            }
+        }
+        return list;
+    }
+}
